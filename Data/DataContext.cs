30842803d86050a5bbf7177b3d3ab495e8d798db
Max Guclu <max.guclu@chasacademy.se>
2023-06-03 00:54:04 +0200

    Add project files.

---
+++ Data/DataContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+
+namespace Filmsystemet.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+        public DbSet<Person> Persons { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Configure the entity mappings and relationships here
+            // if you have any additional configurations.
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ Movies.cs
@@ -0,0 +1,11 @@
+﻿namespace Filmsystemet
+{
+    public class Movies
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Genre { get; set; }
+        public int ReleaseYear { get; set; }
+        public string Description { get; set; }
+    }
+}
